8e58bdefd7f34e9cafc4b6ce8efc34a1cced09fa
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-12 18:33:52 +0530

    Add minCost function for minimum rope connection cost
    Implement a function to calculate the minimum cost of connecting ropes using a priority queue.

---
+++ minmcostofrope07.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// Function to find minimum cost of connecting ropes
+int minCost(vector<int> &ropes) {
+
+    // Create a Min Heap
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    // Insert all rope lengths into the Min Heap
+    for (int rope : ropes) {
+        minHeap.push(rope);
+    }
+
+    int cost = 0;
+
+    // Continue until only one rope remains
+    while (minHeap.size() > 1) {
+
+        // Take two smallest ropes
+        int first = minHeap.top();
+        minHeap.pop();
+
+        int second = minHeap.top();
+        minHeap.pop();
+
+        // Cost of connecting them
+        int sum = first + second;
+
+        // Add to total cost
+        cost += sum;
+
+        // Push newly formed rope back into heap
+        minHeap.push(sum);
+    }
+
+    return cost;
+}
+
+int main() {
+
+    vector<int> ropes = {4, 3, 2, 6};
+
+    cout << "Minimum Cost = " << minCost(ropes);
+
+    return 0;
+}
+// output
+// Minimum cost =29
